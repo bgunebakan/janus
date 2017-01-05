dbbe40cc9eb3915efe8412dc6b37bf8c3114c9d8
Erol Guzoğlu <erolguzoglu@gmail.com>
2017-01-06 02:21:55 +0300

    first commit

---
+++ backend/settings/devl.py
@@ -0,0 +1,29 @@
+import os
+
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'yen5=v7%$s17dl5l3zumezo(m0g6a_c0b8yzr3b$&9p2khj%$!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'janus_dev',
+        'USER': 'janus',
+        'PASSWORD': 'janus',
+        'HOST': '127.0.0.1',
+        }
+}
+
+INTERNAL_IPS = ['192.168.56.1']
+
+INSTALLED_APPS += (
+    'autofixture',
+)
+
+STATICFILES_DIRS.append(
+    os.path.join(BASE_DIR, os.pardir, 'frontend', 'build'),
+)
